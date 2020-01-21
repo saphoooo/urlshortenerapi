2d1ca782df0d47571abc3e3fe60e1847b92c580a
Stephane Beuret <stephane.beuret@data-essential.com>
2020-01-21 11:09:45 +0100

    improve error messages

--- helpers_test.go
+++ helpers_test.go
@@ -9,7 +9,7 @@ func TestBase(t *testing.T) {
 	want := "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	got := base
 	if got != want {
-		t.Errorf("want: %q, but got: %q", want, base)
+		t.Errorf("const base = %q, but want %q", base, want)
 	}
 }
 
@@ -36,13 +36,13 @@ func TestDBGet_string(t *testing.T) {
 	want := mDriver.returnString
 	got, i, err := dbGet(mDriver, want)
 	if got != want {
-		t.Errorf("want: %q, but got: %q", want, got)
+		t.Errorf("dbGet() returned string = %q, want: %q", got, want)
 	}
 	if i != 200 {
-		t.Errorf("want: 200, but got: %d", i)
+		t.Errorf("dbGet() returned int = %d, want: 200 ", i)
 	}
 	if err != nil {
-		t.Errorf("want: nil, but got: %v", err)
+		t.Errorf("dbGet() returned err = %s,want: %s", err, mDriver.returnErr)
 	}
 }
 
@@ -55,14 +55,14 @@ func TestDBGet_err(t *testing.T) {
 	want := mDriver.returnString
 	got, i, err := dbGet(mDriver, want)
 	if got != want {
-		t.Errorf("want: %q, but got: %q", want, got)
+		t.Errorf("dbGet() returned string = %q, want: %q", got, want)
 	}
 	if i != 404 {
-		t.Errorf("want: 404, but got: %d", i)
+		t.Errorf("dbGet() returned int = %d, want: 404 ", i)
 	}
 	if err != nil {
-		if err.Error() != mDriver.returnErr.Error() {
-			t.Errorf("want: %q, but got: %q", mDriver.returnErr, err.Error())
+		if err != mDriver.returnErr {
+			t.Errorf("dbGet() returned err = %s, want: %s", err, mDriver.returnErr)
 		}
 	}
 }
@@ -76,10 +76,10 @@ func TestDBSet_string(t *testing.T) {
 	want := 200
 	got, err := dbSet(mDriver, "foo", "bar")
 	if got != want {
-		t.Errorf("want: %d, but got: %d", want, got)
+		t.Errorf("dbSet() returned int = %d, want: %d ", got, want)
 	}
 	if err != nil {
-		t.Errorf("want: nil, but got: %v", err)
+		t.Errorf("dbSet() returned err = %s, want: %s", err, mDriver.returnErr)
 	}
 }
 
@@ -92,11 +92,11 @@ func TestDBSet_err(t *testing.T) {
 	want := 404
 	got, err := dbSet(mDriver, "foo", "bar")
 	if got != want {
-		t.Errorf("want: %d, but got: %d", want, got)
+		t.Errorf("dbSet() returned int = %d, want: %d ", got, want)
 	}
 	if err != nil {
 		if err.Error() != mDriver.returnErr.Error() {
-			t.Errorf("want: %q, but got: %q", mDriver.returnErr, err.Error())
+			t.Errorf("dbSet() returned err = %s, want: %s", err, mDriver.returnErr)
 		}
 	}
 }
@@ -105,7 +105,7 @@ func TestStrToInt_shortString(t *testing.T) {
 	want := 10
 	got := strToInt("a")
 	if got != want {
-		t.Errorf("want: %d, but got: %d", want, got)
+		t.Errorf("strToInt(shrotString) = %d, want: %d", got, want)
 	}
 }
 
@@ -113,7 +113,16 @@ func TestStrToInt_longString(t *testing.T) {
 	want := strToInt("https://azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN")
 	got := strToInt("https://nbvcxwmlkjhgfdsqpoiuytrezaNBVCXWMLKJHGFDSQPOIUYTREZAnbvcxwmlkjhgfdsqpoiuytrezaNBVCXWMLKJHGFDSQPOIUYTREZAnbvcxwmlkjhgfdsqpoiuytrezaNBVCXWMLKJHGFDSQPOIUYTREZAnbvcxwmlkjhgfdsqpoiuytrezaNBVCXWMLKJHGFDSQPOIUYTREZAnbvcxwmlkjhgfdsqpoiuytrezaNBVCXWMLKJHGFDSQPOIUYTREZAnbvcxwmlkjhgfdsqpoiuytrezaNBVCXWMLKJHGFDSQPOIUYTREZAnbvcxwmlkjhgfdsqpoiuytrezaNBVCXWMLKJHGFDSQPOIUYTREZAnbvcxwmlkjhgfdsqpoiuytrezaNBVCXWMLKJHGFDSQPOIUYTREZAnbvcxwmlkjhgfdsqpoiuytrezaNBVCXWMLKJHGFDSQPOIUYTREZA")
 	if got == want {
-		t.Errorf("%d must be different from %d", want, got)
+		t.Errorf("strToInt(longString) = %d, must be different from %d", got, want)
+	}
+}
+
+func TestStrToInt_idempotence(t *testing.T) {
+	url := "https://www.google.com"
+	want := strToInt(url)
+	got := strToInt(url)
+	if got != want {
+		t.Errorf("strToInt(%s) = %d, want: %d", url, got, want)
 	}
 }
 
