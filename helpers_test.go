dea5ed117ba45c4bfe504e1c1535c064765ac1e3
Stephane Beuret <stephane.beuret@data-essential.com>
2020-01-25 20:35:54 +0100

    some tests

--- helpers_test.go
+++ helpers_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"testing"
 )
 
@@ -13,123 +12,37 @@ func TestBase(t *testing.T) {
 	}
 }
 
-type mockDriver struct {
-	returnString string
-	returnVal    int
-	returnErr    error
-}
-
-func (m *mockDriver) Get(s string) (string, int, error) {
-	return m.returnString, m.returnVal, m.returnErr
-}
-
-func (m *mockDriver) Set(short, long string) (int, error) {
-	return m.returnVal, m.returnErr
-}
-
-func TestDBGet_string(t *testing.T) {
-	mDriver := &mockDriver{
-		returnString: "http://foo.example.com",
-		returnVal:    200,
-		returnErr:    nil,
+func Test_strToInt_basic(t *testing.T) {
+	tests := []struct {
+		name string
+		args string
+		want int
+	}{
+		{"shortString", "a", 10},
+		{"rune", "@#&é(')$*%<>", 2450613165425379093},
 	}
-	want := mDriver.returnString
-	got, i, err := dbGet(mDriver, want)
-	if got != want {
-		t.Errorf("dbGet() returned string = %q, want: %q", got, want)
-	}
-	if i != 200 {
-		t.Errorf("dbGet() returned int = %d, want: 200 ", i)
-	}
-	if err != nil {
-		t.Errorf("dbGet() returned err = %s,want: %s", err, mDriver.returnErr)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := strToInt(tt.args); got != tt.want {
+				t.Errorf("strToInt(%q) = %v, want %v", tt.args, got, tt.want)
+			}
+		})
 	}
 }
 
-func TestDBGet_err(t *testing.T) {
-	mDriver := &mockDriver{
-		returnString: "",
-		returnVal:    404,
-		returnErr:    errors.New("whoops, something wrong happens"),
+func Test_strToInt_long(t *testing.T) {
+	tests := []struct {
+		name string
+		args string
+		want int
+	}{
+		{"longString", "https://nbvcxwmlkjhgfdsqpoiuytrezaNBVCXWMLKJHGFDSQPOIUYTREZAnbvcxwmlkjhgfdsqpoiuytrezaNBVCXWMLKJHGFDSQPOIUYTREZAnbvcxwmlkjhgfdsqpoiuytrezaNBVCXWMLKJHGFDSQPOIUYTREZAnbvcxwmlkjhgfdsqpoiuytrezaNBVCXWMLKJHGFDSQPOIUYTREZAnbvcxwmlkjhgfdsqpoiuytrezaNBVCXWMLKJHGFDSQPOIUYTREZAnbvcxwmlkjhgfdsqpoiuytrezaNBVCXWMLKJHGFDSQPOIUYTREZAnbvcxwmlkjhgfdsqpoiuytrezaNBVCXWMLKJHGFDSQPOIUYTREZAnbvcxwmlkjhgfdsqpoiuytrezaNBVCXWMLKJHGFDSQPOIUYTREZAnbvcxwmlkjhgfdsqpoiuytrezaNBVCXWMLKJHGFDSQPOIUYTREZA", strToInt("https://azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN")},
 	}
-	want := mDriver.returnString
-	got, i, err := dbGet(mDriver, want)
-	if got != want {
-		t.Errorf("dbGet() returned string = %q, want: %q", got, want)
-	}
-	if i != 404 {
-		t.Errorf("dbGet() returned int = %d, want: 404 ", i)
-	}
-	if err != nil {
-		if err != mDriver.returnErr {
-			t.Errorf("dbGet() returned err = %s, want: %s", err, mDriver.returnErr)
-		}
-	}
-}
-
-func TestDBSet_string(t *testing.T) {
-	mDriver := &mockDriver{
-		returnString: "",
-		returnVal:    200,
-		returnErr:    nil,
-	}
-	want := 200
-	got, err := dbSet(mDriver, "foo", "bar")
-	if got != want {
-		t.Errorf("dbSet() returned int = %d, want: %d ", got, want)
-	}
-	if err != nil {
-		t.Errorf("dbSet() returned err = %s, want: %s", err, mDriver.returnErr)
-	}
-}
-
-func TestDBSet_err(t *testing.T) {
-	mDriver := &mockDriver{
-		returnString: "",
-		returnVal:    404,
-		returnErr:    errors.New("whoops, something wrong happens"),
-	}
-	want := 404
-	got, err := dbSet(mDriver, "foo", "bar")
-	if got != want {
-		t.Errorf("dbSet() returned int = %d, want: %d ", got, want)
-	}
-	if err != nil {
-		if err.Error() != mDriver.returnErr.Error() {
-			t.Errorf("dbSet() returned err = %s, want: %s", err, mDriver.returnErr)
-		}
-	}
-}
-
-func TestStrToInt_shortString(t *testing.T) {
-	want := 10
-	got := strToInt("a")
-	if got != want {
-		t.Errorf("strToInt(shrotString) = %d, want: %d", got, want)
-	}
-}
-
-func TestStrToInt_longString(t *testing.T) {
-	want := strToInt("https://azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN")
-	got := strToInt("https://nbvcxwmlkjhgfdsqpoiuytrezaNBVCXWMLKJHGFDSQPOIUYTREZAnbvcxwmlkjhgfdsqpoiuytrezaNBVCXWMLKJHGFDSQPOIUYTREZAnbvcxwmlkjhgfdsqpoiuytrezaNBVCXWMLKJHGFDSQPOIUYTREZAnbvcxwmlkjhgfdsqpoiuytrezaNBVCXWMLKJHGFDSQPOIUYTREZAnbvcxwmlkjhgfdsqpoiuytrezaNBVCXWMLKJHGFDSQPOIUYTREZAnbvcxwmlkjhgfdsqpoiuytrezaNBVCXWMLKJHGFDSQPOIUYTREZAnbvcxwmlkjhgfdsqpoiuytrezaNBVCXWMLKJHGFDSQPOIUYTREZAnbvcxwmlkjhgfdsqpoiuytrezaNBVCXWMLKJHGFDSQPOIUYTREZAnbvcxwmlkjhgfdsqpoiuytrezaNBVCXWMLKJHGFDSQPOIUYTREZA")
-	if got == want {
-		t.Errorf("strToInt(longString) = %d, must be different from %d", got, want)
-	}
-}
-
-func TestStrToInt_idempotence(t *testing.T) {
-	url := "https://www.google.com"
-	want := strToInt(url)
-	got := strToInt(url)
-	if got != want {
-		t.Errorf("strToInt(%s) = %d, want: %d", url, got, want)
-	}
-}
-
-func TestStrToInt_rune(t *testing.T) {
-	want := 0
-	got := strToInt("://*%%*$$^&`")
-	if got == want {
-		t.Errorf("want %d, but got %d", want, got)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := strToInt(tt.args); got == tt.want {
+				t.Errorf("strToInt(%q) = %v, want %v", tt.args, got, tt.want)
+			}
+		})
 	}
 }
